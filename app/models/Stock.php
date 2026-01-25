4bf5e6a6fa2b48bda190b76340c7c0d129b6fc8a
BKBEEZ <bkbeez@gmail.com>
2026-01-25 10:47:49 +0700

    Update Stock.php

--- app/models/Stock.php
+++ app/models/Stock.php
@@ -24,6 +24,19 @@ class Stock extends DB {
     }
 
     /**
+     *  Generate Id
+     *  @return id
+     */
+    static function generateId()
+    {
+        $year = date('Y');
+        $check = Stock::one("SELECT id FROM stock WHERE SUBSTR(id FROM 1 FOR 4)=:check ORDER BY id DESC;", array('check'=>$year));
+        $code = ( (isset($check['id'])&&$check['id']) ? sprintf("%1$04d", intval(str_replace($year,'',$check['id']))+1): '0001' );
+
+        return $year.$code;
+    }
+
+    /**
      *  Type Option
      *  @param  $selected, $addon
      *  @return htmls
