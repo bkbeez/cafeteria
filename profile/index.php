de3198f90a838c24a8c690f051d133a2c23e9656
Somboon Muensrithi <bkbeez@gmail.com>
2026-01-27 14:21:55 +0700

    Update index.php

--- profile/index.php
+++ profile/index.php
@@ -48,121 +48,105 @@
         <div class="row">
             <div class="col-xl-12 mx-auto mt-n19">
                 <div class="card card-profile">
-                <?php if( $shop_id ){ ?>
-                    <div class="row gx-0">
-                        <div class="col-lg-6 image-wrapper bg-image bg-cover rounded-top rounded-lg-start d-md-block" data-image-src="<?=THEME_IMG?>/shop.png"></div>
-                        <div class="col-lg-6">
-                            <div class="p-5 p-md-6 p-lg-7">
-                                <div class="d-flex flex-row mt-3">
-                                    <div><div class="icon me-4 mt-n3" style="font-size:32px;"><i class="uil uil-shop"></i></div></div>
-                                    <div>
-                                        <h5 class="mb-0">
-                                            <?=Lang::get('Shop')?>
-                                            <button type="button" class="btn btn-sm btn-soft-primary rounded-pill" style="padding:3px 6px 3px 4px;line-height:14px;" onclick="profile_events('shop', { 'on':'name' });"><i class="uil uil-pen"></i>&nbsp;<?=Lang::get('Edit')?></button>
-                                        </h5>
-                                        <p class="set-shop-name"><?=((isset($shop['shop_name'])&&$shop['shop_name'])?$shop['shop_name']:'-')?></p>
-                                    </div>
+                    <?php if( $shop_id ){ ?>
+                        <div class="row gx-0">
+                            <div class="col-lg-6 image-wrapper bg-image bg-cover rounded-top rounded-lg-start d-md-block" data-image-src="<?=THEME_IMG?>/shop.png">
+                                <div class="text-center mt-5">
+                                    <mark class="fs-36 text-dark on-bold-primary"><?=((isset($shop['shop_name'])&&$shop['shop_name'])?$shop['shop_name']:'-')?></mark>
                                 </div>
-                                <div class="d-flex flex-row">
-                                    <div><div class="icon me-4 mt-n3" style="font-size:32px;"><i class="uil uil-user"></i></div></div>
-                                    <div>
-                                        <h5 class="mb-0">
-                                            <?=Lang::get('Owner')?>
-                                            <button type="button" class="btn btn-sm btn-soft-primary rounded-pill" style="padding:3px 6px 3px 4px;line-height:14px;" onclick="profile_events('shop', { 'on':'owner' });"><i class="uil uil-pen"></i>&nbsp;<?=Lang::get('Edit')?></button>
-                                        </h5>
-                                        <p class="set-shop-owner"><?=((isset($shop['owner_name'])&&$shop['owner_name'])?$shop['owner_name']:'-')?></p>
-                                    </div>
-                                </div>
-                                <div class="d-flex flex-row">
-                                    <div><div class="icon me-4 mt-n3" style="font-size:32px;"><i class="uil uil-credit-card"></i></div></div>
-                                    <div>
-                                        <h5 class="mb-0">
-                                            <?=Lang::get('TaxNumber')?>
-                                            <button type="button" class="btn btn-sm btn-soft-primary rounded-pill" style="padding:3px 6px 3px 4px;line-height:14px;" onclick="profile_events('shop', { 'on':'tax' });"><i class="uil uil-pen"></i>&nbsp;<?=Lang::get('Edit')?></button>
-                                        </h5>
-                                        <p class="set-shop-tax"><?=((isset($shop['tax_number'])&&$shop['tax_number'])?'<mark class="doc tax-number"><span class="text-white bg-primary">TAX</span>'.$shop['tax_number'].'</mark>':'-')?></p>
-                                    </div>
-                                </div>
-                                <div class="d-flex flex-row">
-                                    <div><div class="icon me-4 mt-n3" style="font-size:32px;"><i class="uil uil-postcard"></i></div></div>
-                                    <div>
-                                        <h5 class="mb-0">
-                                            <?=Lang::get('AddressBilling')?>
-                                            <button type="button" class="btn btn-sm btn-soft-primary rounded-pill" style="padding:3px 6px 3px 4px;line-height:14px;" onclick="profile_events('shop', { 'on':'address' });"><i class="uil uil-pen"></i>&nbsp;<?=Lang::get('Edit')?></button>
-                                        </h5>
-                                        <p class="set-shop-address"><?=((isset($shop['fulladdress'])&&$shop['fulladdress'])?$shop['fulladdress']:'-')?></p>
-                                    </div>
-                                </div>
-                                <div class="d-flex flex-row">
-                                    <div><div class="icon me-4 mt-n3" style="font-size:32px;"><i class="uil uil-phone-volume"></i></div></div>
-                                    <div>
-                                        <h5 class="mb-0">
-                                            <?=Lang::get('Phone')?>
-                                            <button type="button" class="btn btn-sm btn-soft-primary rounded-pill" style="padding:3px 6px 3px 4px;line-height:14px;" onclick="profile_events('shop', { 'on':'phone' });"><i class="uil uil-pen"></i>&nbsp;<?=Lang::get('Edit')?></button>
-                                        </h5>
-                                        <p class="set-shop-phone"><?=((isset($shop['phone'])&&$shop['phone'])?$shop['phone']:'-')?></p>
-                                    </div>
-                                </div>
-                                <div class="d-flex flex-row">
-                                    <div><div class="icon me-4 mt-n3" style="font-size:32px;"><i class="uil uil-envelopes"></i></div></div>
-                                    <div>
-                                        <h5 class="mb-0">
-                                            <?=Lang::get('Email')?>
-                                        </h5>
-                                        <p class="set-user-email"><?=User::get('email', '-').( (User::get('is_cmu')=='Y') ? '<br>'.User::get('email_cmu') : null )?></p>
-                                    </div>
-                                </div>
-                                <button type="button" class="btn btn-primary rounded w-100" onclick="document.location='<?=APP_HOME?>/request';">BOOKING &rarr;</button>
                             </div>
-                        </div>
-                    </div>
-                <?php }else{ ?>
-                    <div class="row gx-0">
-                        <div class="col-lg-6 image-wrapper bg-image bg-cover rounded-top rounded-lg-start d-md-block" data-image-src="<?=THEME_IMG?>/cafeteria.jpg"></div>
-                        <div class="col-lg-6">
-                            <div class="p-5 p-md-6 p-lg-7">
-                                <div class="d-flex flex-row mt-3">
-                                    <div><div class="icon me-4 mt-n3" style="font-size:32px;"><i class="uil uil-shield-check"></i></div></div>
-                                    <div>
-                                        <h5 class="mb-0">
-                                            <?=Lang::get('UserAccount')?>
-                                        </h5>
-                                        <p class="set-user-email"><?=Util::memberRoleName(User::get('role'))?></p>
+                            <div class="col-lg-6">
+                                <div class="p-5 p-md-6 p-lg-7">
+                                    <div class="d-flex flex-row mt-3">
+                                        <div><div class="icon me-4 mt-n3" style="font-size:32px;"><i class="uil uil-user"></i></div></div>
+                                        <div>
+                                            <h5 class="mb-0">
+                                                <?=Lang::get('Owner')?>
+                                                <button type="button" class="btn btn-sm btn-soft-primary rounded-pill" style="padding:3px 6px 3px 4px;line-height:14px;" onclick="profile_events('shop', { 'on':'owner' });"><i class="uil uil-pen"></i>&nbsp;<?=Lang::get('Edit')?></button>
+                                            </h5>
+                                            <p class="set-shop-owner"><?=((isset($shop['owner_name'])&&$shop['owner_name'])?$shop['owner_name']:'-')?></p>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="d-flex flex-row">
-                                    <div><div class="icon me-4 mt-n3" style="font-size:32px;"><i class="uil uil-user"></i></div></div>
-                                    <div>
-                                        <h5 class="mb-0">
-                                            <?=Lang::get('NameFull')?>
-                                            <button type="button" class="btn btn-sm btn-soft-primary rounded-pill" style="padding:3px 6px 3px 4px;line-height:14px;" onclick="profile_events('user', { 'on':'name' });"><i class="uil uil-pen"></i>&nbsp;<?=Lang::get('Edit')?></button>
-                                        </h5>
-                                        <p class="set-user-name"><?=User::get('fullname', '-')?></p>
+                                    <div class="d-flex flex-row">
+                                        <div><div class="icon me-4 mt-n3" style="font-size:32px;"><i class="uil uil-credit-card"></i></div></div>
+                                        <div>
+                                            <h5 class="mb-0">
+                                                <?=Lang::get('TaxNumber')?>
+                                                <button type="button" class="btn btn-sm btn-soft-primary rounded-pill" style="padding:3px 6px 3px 4px;line-height:14px;" onclick="profile_events('shop', { 'on':'tax' });"><i class="uil uil-pen"></i>&nbsp;<?=Lang::get('Edit')?></button>
+                                            </h5>
+                                            <p class="set-shop-tax"><?=((isset($shop['tax_number'])&&$shop['tax_number'])?'<mark class="doc tax-number"><span class="text-white bg-primary">TAX</span>'.$shop['tax_number'].'</mark>':'-')?></p>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="d-flex flex-row">
-                                    <div><div class="icon me-4 mt-n3" style="font-size:32px;"><i class="uil uil-phone-volume"></i></div></div>
-                                    <div>
-                                        <h5 class="mb-0">
-                                            <?=Lang::get('Phone')?>
-                                            <button type="button" class="btn btn-sm btn-soft-primary rounded-pill" style="padding:3px 6px 3px 4px;line-height:14px;" onclick="profile_events('user', { 'on':'phone' });"><i class="uil uil-pen"></i>&nbsp;<?=Lang::get('Edit')?></button>
-                                        </h5>
-                                        <p class="set-user-phone"><?=User::get('phone', '-')?></p>
+                                    <div class="d-flex flex-row">
+                                        <div><div class="icon me-4 mt-n3" style="font-size:32px;"><i class="uil uil-postcard"></i></div></div>
+                                        <div>
+                                            <h5 class="mb-0">
+                                                <?=Lang::get('AddressBilling')?>
+                                                <button type="button" class="btn btn-sm btn-soft-primary rounded-pill" style="padding:3px 6px 3px 4px;line-height:14px;" onclick="profile_events('shop', { 'on':'address' });"><i class="uil uil-pen"></i>&nbsp;<?=Lang::get('Edit')?></button>
+                                            </h5>
+                                            <p class="set-shop-address"><?=((isset($shop['fulladdress'])&&$shop['fulladdress'])?$shop['fulladdress']:'-')?></p>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="d-flex flex-row">
-                                    <div><div class="icon me-4 mt-n3" style="font-size:32px;"><i class="uil uil-envelopes"></i></div></div>
-                                    <div>
-                                        <h5 class="mb-0">
-                                            <?=Lang::get('Email')?>
-                                        </h5>
-                                        <p class="set-user-email"><?=User::get('email', '-').( (User::get('is_cmu')=='Y') ? '<br>'.User::get('email_cmu') : null )?></p>
+                                    <div class="d-flex flex-row">
+                                        <div><div class="icon me-4 mt-n3" style="font-size:32px;"><i class="uil uil-phone-volume"></i></div></div>
+                                        <div>
+                                            <h5 class="mb-0">
+                                                <?=Lang::get('Phone')?>
+                                                <button type="button" class="btn btn-sm btn-soft-primary rounded-pill" style="padding:3px 6px 3px 4px;line-height:14px;" onclick="profile_events('shop', { 'on':'phone' });"><i class="uil uil-pen"></i>&nbsp;<?=Lang::get('Edit')?></button>
+                                            </h5>
+                                            <p class="set-shop-phone"><?=((isset($shop['phone'])&&$shop['phone'])?$shop['phone']:'-')?></p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                <?php } ?>
+                    <?php }else{ ?>
+                        <div class="row gx-0">
+                            <div class="col-lg-6 image-wrapper bg-image bg-cover rounded-top rounded-lg-start d-md-block" data-image-src="<?=THEME_IMG?>/cafeteria.jpg"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5 p-md-6 p-lg-7">
+                                    <div class="d-flex flex-row mt-3">
+                                        <div><div class="icon me-4 mt-n3" style="font-size:32px;"><i class="uil uil-shield-check"></i></div></div>
+                                        <div>
+                                            <h5 class="mb-0">
+                                                <?=Lang::get('UserAccount')?>
+                                            </h5>
+                                            <p class="set-user-email"><?=Util::memberRoleName(User::get('role'))?></p>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row">
+                                        <div><div class="icon me-4 mt-n3" style="font-size:32px;"><i class="uil uil-user"></i></div></div>
+                                        <div>
+                                            <h5 class="mb-0">
+                                                <?=Lang::get('NameFull')?>
+                                                <button type="button" class="btn btn-sm btn-soft-primary rounded-pill" style="padding:3px 6px 3px 4px;line-height:14px;" onclick="profile_events('user', { 'on':'name' });"><i class="uil uil-pen"></i>&nbsp;<?=Lang::get('Edit')?></button>
+                                            </h5>
+                                            <p class="set-user-name"><?=User::get('fullname', '-')?></p>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row">
+                                        <div><div class="icon me-4 mt-n3" style="font-size:32px;"><i class="uil uil-phone-volume"></i></div></div>
+                                        <div>
+                                            <h5 class="mb-0">
+                                                <?=Lang::get('Phone')?>
+                                                <button type="button" class="btn btn-sm btn-soft-primary rounded-pill" style="padding:3px 6px 3px 4px;line-height:14px;" onclick="profile_events('user', { 'on':'phone' });"><i class="uil uil-pen"></i>&nbsp;<?=Lang::get('Edit')?></button>
+                                            </h5>
+                                            <p class="set-user-phone"><?=User::get('phone', '-')?></p>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row">
+                                        <div><div class="icon me-4 mt-n3" style="font-size:32px;"><i class="uil uil-envelopes"></i></div></div>
+                                        <div>
+                                            <h5 class="mb-0">
+                                                <?=Lang::get('Email')?>
+                                            </h5>
+                                            <p class="set-user-email"><?=User::get('email', '-').( (User::get('is_cmu')=='Y') ? '<br>'.User::get('email_cmu') : null )?></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
                 </div>
             </div>
         </div>
