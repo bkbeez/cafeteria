6df972621c5ff51d1e10e9ba5894d4669d6ba61a
Somboon Muensrithi <bkbeez@gmail.com>
2026-01-28 13:36:53 +0700

    Update admin

--- admin/users/filter/new.php
+++ admin/users/filter/new.php
@@ -94,7 +94,7 @@
                         </div>
                         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
                             <div class="form-floating form-select-wrapper mb-1">
-                                <select id="shop_id" name="shop_id" class="form-select" aria-label="..."><?=Shop::getOption(null, '<option value="">'.Lang::get('Empty').'...</option>')?></select>
+                                <select id="shop_id" name="shop_id" class="form-select" aria-label="..."><?=Shop::getOptionAll(null, '<option value="">'.Lang::get('Empty').'...</option>')?></select>
                                 <label for="shop_id"><?=Lang::get('Shop')?></label>
                             </div>
                         </div>
